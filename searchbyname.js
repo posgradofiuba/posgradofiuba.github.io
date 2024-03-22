a59625405442cf1542727ffb8126ebd95e05443c
Tom <tomyadrover@gmail.com>
2024-03-22 12:32:25 -0300

    Update searchbyname.js

--- searchbyname.js
+++ searchbyname.js
@@ -16,15 +16,16 @@ function searchByName() {
             links.forEach(function(link) {
                 var linkText = link.innerText.trim().toLowerCase();
                 var href = link.getAttribute("href");
+                var resolutionNumber = linkText.match(/\d+\/\d+/)[0]; // Extraer el número de resolución del texto del enlace
 
-                if (linkText.includes(searchTerm)) { // Comprobamos si el nombre de la carrera incluye el término de búsqueda
+                if (h3Text.toLowerCase().includes(searchTerm)) { // Comprobamos si el nombre de la carrera incluye el término de búsqueda
                     var resultLink = document.createElement("a");
                     resultLink.href = href;
-                    resultLink.textContent = linkText;
+                    resultLink.textContent = resolutionNumber; // Mostrar el número de resolución como texto del enlace
                     resultLink.target = "_blank";
 
                     var resultItem = document.createElement("p");
-                    resultItem.innerHTML = "<strong>" + h2Text + " - " + h3Text + ": </strong>";
+                    resultItem.innerHTML = "<strong>" + h2Text + " - " + h3Text + ": </strong>"; // Mostrar solo el nombre de la carrera
                     resultItem.appendChild(resultLink);
 
                     searchResults.push(resultItem);
