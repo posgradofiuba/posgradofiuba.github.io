6fc8c60c377ce3a5625b2efa3549d1f4ccdb7f97
Tom <tomyadrover@gmail.com>
2024-03-22 17:31:16 -0300

    Update searchByName.js

--- searchByName.js
+++ searchByName.js
@@ -1,36 +1,34 @@
-function searchByName() {
-    var searchTerm = document.getElementById("searchByNameInput").value.trim().toLowerCase(); // Convertir el término de búsqueda a minúsculas
+function search() {
+    var searchTerm = document.getElementById("searchInput").value.trim().toLowerCase();
     if (!searchTerm) return;
 
     var sections = document.querySelectorAll("section");
     var searchResults = [];
 
     sections.forEach(function(section) {
-        var h3 = section.querySelectorAll("h3");
-        h3.forEach(function(h3Element) {
-            var h3Text = h3Element.innerText.trim().toLowerCase(); // Convertir el texto del h3 a minúsculas
-            if (h3Text.includes(searchTerm)) {
-                var links = h3Element.nextElementSibling.querySelectorAll("a");
-                links.forEach(function(link) {
-                    var href = link.getAttribute("href");
-                    var linkText = link.innerText.trim().toLowerCase(); // Convertir el texto del enlace a minúsculas
-                    
-                    var resultLink = document.createElement("a");
-                    resultLink.href = href;
-                    resultLink.textContent = linkText; // Mantener el texto del enlace en minúsculas
-                    resultLink.target = "_blank";
+        var links = section.querySelectorAll("a");
+        links.forEach(function(link) {
+            var linkText = link.innerText.trim().toLowerCase();
+            var href = link.getAttribute("href");
+            var h3Text = link.closest("ul").previousElementSibling.innerText.trim();
 
-                    var resultItem = document.createElement("p");
-                    resultItem.innerHTML = "<strong>" + h3Element.innerText.trim() + ": </strong>"; // Usar el texto original del h3
-                    resultItem.appendChild(resultLink);
+            if (startsWithCaseInsensitive(linkText, searchTerm)) {
+                var resultLink = document.createElement("a");
+                resultLink.href = href;
+                resultLink.textContent = linkText;
+                resultLink.target = "_blank";
 
-                    searchResults.push(resultItem);
-                });
+                var trimmedH3Text = h3Text.replace("Laboratorio de Sistemas Embebidos (LSE) - ", "");
+                var resultItem = document.createElement("p");
+                resultItem.innerHTML = "<strong>" + trimmedH3Text + ": </strong>";
+                resultItem.appendChild(resultLink);
+
+                searchResults.push(resultItem);
             }
         });
     });
 
-    var searchResultsElement = document.getElementById("searchResultsByName");
+    var searchResultsElement = document.getElementById("searchResults");
     searchResultsElement.innerHTML = "";
     if (searchResults.length > 0) {
         searchResults.forEach(function(result) {
@@ -39,4 +37,11 @@ function searchByName() {
     } else {
         searchResultsElement.innerHTML = "<p>No se encontraron resultados.</p>";
     }
+}
+
+function startsWithCaseInsensitive(text, search) {
+    // Normalizamos los textos antes de comparar
+    var normalizedText = unorm.nfd(text).toLowerCase();
+    var normalizedSearch = unorm.nfd(search).toLowerCase();
+    return normalizedText.startsWith(normalizedSearch);
 }
