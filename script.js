b23165ef35496f69d24691d019060b8b18ace1e2
Tom <tomyadrover@gmail.com>
2024-03-23 14:46:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -12,8 +12,3 @@ function scrollToTop() {
     document.body.scrollTop = 0; // Para Safari
     document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
 }
-
-// Verificar si el navegador o el sistema operativo del usuario tiene configurado el modo oscuro
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-    document.body.classList.add('dark-mode'); // Aplicar modo oscuro al cuerpo del documento
-}
