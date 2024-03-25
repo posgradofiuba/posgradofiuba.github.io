6bb3c79d37b618b9a62ec19114736f6679260bfe
Tom <tomyadrover@gmail.com>
2024-03-25 11:04:50 -0300

    Update searchByName.js

--- searchByName.js
+++ searchByName.js
@@ -1,13 +1,14 @@
-function normalizeText(text) {
+searchbyname.js, function normalizeText(text) {
     console.log("Normalized text: ", text);
     return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
 }
 
 function searchByName() {
     console.log("searchByName() function called");
-    var searchTerm = normalizeText(document.getElementById("searchByNameInput").value.trim());
+    var searchTerm = normalizeText(document.getElementById("searchByNameInput").value.trim()); // Normalizar el término de búsqueda
     var searchResultsElement = document.getElementById("searchResultsByName");
 
+    // Limpiar los resultados si el campo de búsqueda está vacío
     if (!searchTerm) {
         searchResultsElement.innerHTML = "";
         return;
@@ -19,20 +20,20 @@ function searchByName() {
     sections.forEach(function(section) {
         var h3 = section.querySelectorAll("h3");
         h3.forEach(function(h3Element) {
-            var h3Text = normalizeText(h3Element.innerText.trim());
+            var h3Text = normalizeText(h3Element.innerText.trim()); // Normalizar el texto del h3
             if (h3Text.includes(searchTerm)) {
                 var links = h3Element.nextElementSibling.querySelectorAll("a");
                 links.forEach(function(link) {
                     var href = link.getAttribute("href");
-                    var linkText = normalizeText(link.innerText.trim());
+                    var linkText = normalizeText(link.innerText.trim()); // Normalizar el texto del enlace
                     
                     var resultLink = document.createElement("a");
                     resultLink.href = href;
-                    resultLink.textContent = linkText;
+                    resultLink.textContent = linkText; // Mantener el texto del enlace
                     resultLink.target = "_blank";
 
                     var resultItem = document.createElement("p");
-                    resultItem.innerHTML = "<strong>" + h3Element.innerText.trim() + ": </strong>";
+                    resultItem.innerHTML = "<strong>" + h3Element.innerText.trim() + ": </strong>"; // Usar el texto original del h3
                     resultItem.appendChild(resultLink);
 
                     searchResults.push(resultItem);
