80c1336c916d53db21faa154e26691fd8af9b4d7
Tom <tomyadrover@gmail.com>
2024-04-09 13:09:49 -0300

    Update script.js

--- resoluciones/script.js
+++ resoluciones/script.js
@@ -1,4 +1,6 @@
-window.onscroll = function() { scrollFunction() };
+window.onscroll = function() { 
+    scrollFunction();
+};
 
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -9,6 +11,13 @@ function scrollFunction() {
 }
 
 function scrollToTop() {
+    // Eliminar el numeral del enlace cuando se hace clic en el botón de subir
+    const enlaces = document.querySelectorAll('.indice a');
+    enlaces.forEach(function(enlace) {
+        enlace.href = enlace.href.replace(/#.*$/, '');
+    });
+
+    // Hacer scroll hasta arriba de la página
     document.body.scrollTop = 0; // Para Safari
     document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
 }
