86d44561481b161607f3e9f92560c9b5320f5d70
virulopez <163030843+virulopez@users.noreply.github.com>
2024-04-22 14:18:12 -0300

    Update script.js

--- egresados/script.js
+++ egresados/script.js
@@ -107,5 +107,12 @@ particlesJS("particles-js", {
       }
     },
     "retina_detect": true
-  });
-  
+  }
+
+  function scrollToTop() {
+    document.body.scrollTop = 0; // Para Safari
+    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+}          
+      );
+
+
