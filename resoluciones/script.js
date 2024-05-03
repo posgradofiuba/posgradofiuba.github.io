224bedce456a3740b8ce54987185f140f8f39894
Tom <tomyadrover@gmail.com>
2024-05-03 19:26:10 -0300

    Update script.js

--- resoluciones/script.js
+++ resoluciones/script.js
@@ -9,6 +9,23 @@ function scrollFunction() {
 }
 
 function scrollToTop() {
-    document.body.scrollTop = 0; // Para Safari
-    document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+function toggleContent() {
+    var content = document.getElementById("contentToHide");
+    var toggleButton = document.getElementById("toggleButton");
+    var toggleIcon = document.getElementById("toggleIcon");
+    var displayValue = window.getComputedStyle(content).getPropertyValue('display');
+
+    if (displayValue === "none") {
+    content.style.display = "block";
+    toggleIcon.src = "https://i.imgur.com/SqonsjV.png";
+    toggleButton.setAttribute("aria-label", "Ocultar Resoluciones");
+    } else {
+    content.style.display = "none";
+    toggleIcon.src = "https://i.imgur.com/lnHhdlc.png";
+    toggleButton.setAttribute("aria-label", "Mostrar Resoluciones");
+    }
 }
