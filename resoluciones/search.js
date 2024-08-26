ade7d56d707330d897ecdf58eecb63a6e7f454ad
Tom <tomyadrover@gmail.com>
2024-08-26 10:24:19 -0300

    Update search.js

--- resoluciones/search.js
+++ resoluciones/search.js
@@ -9,29 +9,35 @@ function search() {
 
     var sections = document.querySelectorAll("section");
     var searchResults = [];
+    
     sections.forEach(function(section) {
         var links = section.querySelectorAll("a");
+        
         links.forEach(function(link) {
             var linkText = link.innerText.trim().toLowerCase();
             var href = link.getAttribute("href");
-            var h3Text = link.closest("ul").previousElementSibling.innerText.trim();
             
             if (linkText.includes(searchTerm)) {
-                var resultLink = document.createElement("a");
-                resultLink.href = href;
-                resultLink.textContent = linkText;
-                resultLink.target = "_blank";
-                var trimmedH3Text = h3Text.replace("Laboratorio de Sistemas Embebidos (LSE) - ", "");
-                var resultItem = document.createElement("p");
-                resultItem.innerHTML = "<strong>" + trimmedH3Text + ": </strong>";
-                resultItem.appendChild(resultLink);
-                searchResults.push(resultItem);
+                var ulElement = link.closest("ul");
+                if (ulElement && ulElement.previousElementSibling) {
+                    var h3Text = ulElement.previousElementSibling.innerText.trim();
+                    var resultLink = document.createElement("a");
+                    resultLink.href = href;
+                    resultLink.textContent = linkText;
+                    resultLink.target = "_blank";
+                    var trimmedH3Text = h3Text.replace("Laboratorio de Sistemas Embebidos (LSE) - ", "");
+                    var resultItem = document.createElement("p");
+                    resultItem.innerHTML = "<strong>" + trimmedH3Text + ": </strong>";
+                    resultItem.appendChild(resultLink);
+                    searchResults.push(resultItem);
+                }
             }
         });
     });
 
+    searchResultsElement.innerHTML = ""; // Clear results once before appending new ones
+
     if (searchResults.length > 0) {
-        searchResultsElement.innerHTML = "";
         searchResults.forEach(function(result) {
             searchResultsElement.appendChild(result);
         });
